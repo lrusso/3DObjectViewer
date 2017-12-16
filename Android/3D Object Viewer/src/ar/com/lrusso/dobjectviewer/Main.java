63b1b9defb7ba39f7bcc43a0b4112b20a4ab77e7
Leonardo Javier Russo <lrusso@Mac-Mini.local>
2017-12-16 13:20:56 -0300

    -
    -

--- Android/3D Object Viewer/src/ar/com/lrusso/dobjectviewer/Main.java
+++ Android/3D Object Viewer/src/ar/com/lrusso/dobjectviewer/Main.java
@@ -1,7 +1,5 @@
 package ar.com.lrusso.dobjectviewer;
 
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.widget.PopupMenu;
 import android.text.Html;
 
 import java.io.BufferedReader;
@@ -12,6 +10,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.Calendar;
 
+import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
@@ -28,9 +27,10 @@ import android.webkit.ValueCallback;
 import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.widget.EditText;
+import android.widget.PopupMenu;
 import android.widget.TextView;
 
-public class Main extends ActionBarActivity
+public class Main extends Activity
 	{
 	private WebView webView;
 	private ValueCallback<Uri> mUploadMessage;  
